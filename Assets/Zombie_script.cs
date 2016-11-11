858d3ca1cce1b9c62017ecbc0a8f44ff0d8612af
jeffjose95 <Jeff_jose95@yahoo.com>
2016-11-11 14:33:03 -0800

    Asset Update
    Included Asset folder

---
+++ Assets/Zombie_script.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zombie_script : MonoBehaviour {
+	Transform Player;
+    SphereCollider sphere;
+	NavMeshAgent nav;
+    Animator zombie;
+	// Use this for initialization
+	void Awake () 
+	{
+		Player = GameObject.FindGameObjectWithTag ("Player").transform;
+		nav = GetComponent<NavMeshAgent> ();
+        sphere = GetComponent<SphereCollider>();
+        zombie = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		nav.SetDestination (Player.position);
+      
+	}
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            zombie.Play("attack", -1);
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            zombie.Play("walk", -1);
+        }
+    }
+}
